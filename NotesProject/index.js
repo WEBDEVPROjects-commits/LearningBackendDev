41f8fd6852c84bc63c5a63a24cf9833c43de7aa3
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-04-01 23:09:50 +0530

    add task to real files in system

--- NotesProject/index.js
+++ NotesProject/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
-
+const fs=require('fs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
@@ -10,8 +10,21 @@ app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine','ejs')
 
 app.get("/",(req,res) => {
-    res.render("index")
+    fs.readdir('./files',(err,files) => {
+        res.render("index",{files:files});
+    })
 })
-
+app.post("/create",(req,res) => {
+    fs.writeFile(`./files/${req.body.title.split(' ').join("")}.txt`,`${req.body.details}`,(err) => {
+        err?console.error(err):console.log("File succesfully created");
+        res.redirect("/")
+    })
+})
+app.get("/files/:file",(req,res) => {
+        fs.readFile(`./files/${req.params.file}`,(err,data) => {
+            err?console.error(err):console.log("Successfully reading the file")
+            res.render("show",{data:data,file:req.params.file})
+        })
+})
 
 app.listen(3000)
