a97535ea8ce9d1c28b2975eed310e78b8bcd99a1
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-04-03 17:37:06 +0530

    edit file name feature added

--- NotesProject/index.js
+++ NotesProject/index.js
@@ -26,5 +26,16 @@ app.get("/files/:file",(req,res) => {
             res.render("show",{data:data,file:req.params.file})
         })
 })
+app.get("/edit/:file",(req,res) => {
+        // // console.log(req.params.file)
+        res.render("edit",{filename:req.params.file});
+})
+app.post("/update",(req,res) => {
+        console.log(req.body)
+        fs.rename(`files/${req.body.previous}`,`files/${req.body.new}.txt`,(err) => {
+            err?console.error(err):console.log("File successfully edited")
+        })
+        res.redirect("/");
+})
 
 app.listen(3000)
