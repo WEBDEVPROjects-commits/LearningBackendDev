8ffc8ab27c297e64a99f841fa609302b3e89785f
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-04-14 23:38:07 +0530

    Update implemented in mongodb

--- MongoDb12/app.js
+++ MongoDb12/app.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
-const userModel=require('./userModel')
+const userModel=require('./models/userModel')
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
@@ -11,20 +11,37 @@ app.set("view engine","ejs")
 
 app.get("/",async (req,res) => {
     let users=await userModel.find();
-    res.render("index",{users:users});  
+    res.render("index",{users:users});
 })
 app.post("/create",async (req,res) => {
-
+    let {username,age,email,phone}=req.body;
     let createdUser=await userModel.create(
         {
-            username:req.body.username,
-            age:req.body.age,
-            email:req.body.email,
-            phone:req.body.phone,
+            username:username,
+            age:age,
+            email:email,
+            phone:phone,
         }
     )
     res.redirect("/")
 })
+app.get("/delete/:id",async (req,res) => {
+        await userModel.findOneAndDelete({ _id:req.params.id});
+        res.redirect("/")
+})
+
+app.get("/update/:id",(req,res) => {
+    res.render("update",{id:req.params.id});
+})
+app.post("/updated/:id",async (req,res) => {
+    let {username,age,email,phone}=req.body;
+    await userModel.findOneAndUpdate({_id:req.params.id},{username:username,age:age,email:email,phone:phone},{new:true})
+    res.redirect("/");
+})
+app.get("/read",async (req,res) => {
+    let users=await userModel.find();
+    res.send(users);
+})
 app.listen(3000,(err) => {
-        err?console.error(err):console.log("Port succesfully running")
+        err?console.error(err):console.log("Port sucessfully running")
 })
