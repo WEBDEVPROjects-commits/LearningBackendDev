b2ce1661743ef6f866b430bb657ee3e1ec375249
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-05-09 13:49:04 +0530

    Register,Login,Logout implemented

--- MongoDb15/app.js
+++ MongoDb15/app.js
@@ -9,6 +9,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const { hash } = require('crypto');
 app.set("view engine", "ejs");
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,26 +20,24 @@ app.get("/", (req, res) => {
 
 app.post("/create", async (req, res) => {
     console.log(req.body);
-    let { Name, Username, Email, PhoneNumber, Password, ConfirmPassword,Gender, Dob } = req.body
+    let { Name, Username, Email, PhoneNumber, Password, ConfirmPassword, Gender, Dob } = req.body
 
-     const salt = await bcrypt.genSalt(10);
-     const hashedPassword = await bcrypt.hash(Password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(Password, salt);
 
     const createdUser = await userModel.create({
         Name,
         Username,
         Email,
         PhoneNumber,
-        Password:hashedPassword,
+        Password: hashedPassword,
         PhoneNumber,
         Gender,
         Dob
     });
-    const token=jwt.sign({email: Email},"secret");
-    res.cookie("token",token);
     res.redirect("/login");
 })
-app.get("/login",(req,res) => {
+app.get("/login", (req, res) => {
     // let {Email,Password}=req.params;
     // res.render("login",{Email,Password})
     res.render("login");
@@ -48,32 +47,36 @@ app.get("/login",(req,res) => {
 //   res.next();
 
 // })//error handling middleware function
-app.post("/login",async (req,res) => {
+app.post("/login", async (req, res) => {
     console.log(req.body)
-    let {Email,Password}=req.body;
-    const requiredUser=await userModel.findOne({Email:Email});
+    let { Email, Password } = req.body;
+    const requiredUser = await userModel.findOne({ Email: Email });
     // if(!requiredUser)
-        if(!requiredUser){
-            res.send("Something went wrong");
-        }
-        else{
-        if((requiredUser.Email===Email)){
-               const result=await bcrypt.compare(Password,requiredUser.Password)
-                if(result==true){
-                    res.send("Logged in successfully");
-                }
-                else{
-                    res.send("Entered email or username is incorrect");
-                }
-        }
-        }
+    if (!requiredUser) {
+        res.send("Something went wrong");
+    }
+    else {
+            const result = await bcrypt.compare(Password, requiredUser.Password)
+            if (result === true) {
+                const token = jwt.sign({email:Email }, "secret");
+                res.cookie("token", token);
+                res.send("Logged in successfully");
+            }
+            else {
+                res.send("Entered email or password is incorrect");
+            }  
+    }
 })
-app.get("/delete",async (req,res) => {
-    const users=await userModel.deleteMany({});
+app.get("/logout",(req,res)=>{
+    res.clearCookie('token');
+    res.redirect("/login");
+})
+app.get("/delete", async (req, res) => {
+    const users = await userModel.deleteMany({});
     res.send(users);
 })
-app.get("/read",async (req,res) => {
-    const users=await userModel.find();
+app.get("/read", async (req, res) => {
+    const users = await userModel.find();
     res.send(users);
 })
 app.listen(3000);
